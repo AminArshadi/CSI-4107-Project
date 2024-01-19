c49a166d2b8733a58a3b20c4271e4b539de6e176
Amin Arshadi <aminarshadi@amins-mbp.home>
2024-01-18 22:09:15 -0500

    assignment file updated

--- assignment_1.py
+++ assignment_1.py
@@ -1,8 +1,38 @@
 import spacy
+import re
 
 def read_doc(file_path):
     with open(file_path, 'r') as file:
         return file.read()
+
+def parse_document(file_path):
+    content = read_doc(file_path)
+
+    documents = []
+    doc_splits = content.split('<DOC>')
+    
+    for doc in doc_splits:
+        if doc.strip():
+            
+            docno = re.search('<DOCNO>(.*?)</DOCNO>', doc, re.DOTALL)
+            fileid = re.search('<FILEID>(.*?)</FILEID>', doc, re.DOTALL)
+            headline = re.search('<HEAD>(.*?)</HEAD>', doc, re.DOTALL)
+            text = re.search('<TEXT>(.*?)</TEXT>', doc, re.DOTALL)
+            
+            docno = docno.group(1).strip() if docno else None
+            fileid = fileid.group(1).strip() if fileid else None
+            headline = headline.group(1).strip() if headline else None
+            text = text.group(1).strip() if text else None
+            
+            info = {
+                'docno':docno,
+                'fileid':fileid,
+                'headline':headline,
+                'text':text,}
+
+            documents.append(info)
+
+    return documents
 
 def tokenize_and_lemmatize(text):
     # Load the English model
